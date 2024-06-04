3e842b435dc8b0c7ba73a2ff823a1335d75713a6
Christian Contreras <82068788+degoleiv@users.noreply.github.com>
2024-06-04 15:35:08 -0500

    configurando imagen

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ export function Header() {
     return (
         <header className="header">
             <div className="header-content">
-                <img src="https://www.google.com/imgres?q=christian%20pool%20contreras&imgurl=https%3A%2F%2Fmedia.licdn.com%2Fdms%2Fimage%2FD5603AQFv71xCFtL4LA%2Fprofile-displayphoto-shrink_200_200%2F0%2F1710393763804%3Fe%3D2147483647%26v%3Dbeta%26t%3D4rF-tGEzSdO-dcnG2lZp8BnG3LtdHY612c3sqYFckBs&imgrefurl=https%3A%2F%2Fco.linkedin.com%2Fin%2Fchristiancontreras-gamedev&docid=y7auvKG8zUII9M&tbnid=muzfp7S7Tgu--M&vet=12ahUKEwiYloX05MKGAxUsQzABHYqzDEAQM3oECBgQAA..i&w=200&h=200&hcb=2&ved=2ahUKEwiYloX05MKGAxUsQzABHYqzDEAQM3oECBgQAA" alt="photo" className="header-image" />
+                <img src=".\src\assets\1710393763804.jpg" alt="photo" className="header-image" />
                 <div className="header-text">
                     <h1>CHRISTIAN CONTRERAS</h1>
                     <h2>GAME DEVELOPER AND SOFTWARE DEVELOPER</h2>
