a0f3322eca486d201af0f5637284640acc80db28
Christian Contreras <82068788+degoleiv@users.noreply.github.com>
2024-06-04 20:23:16 -0500

    imagen drive

--- src/Components/HeaderTitle.jsx
+++ src/Components/HeaderTitle.jsx
@@ -4,7 +4,7 @@ export default function Header({title, subtitle, description}) {
     return (
         <header className="header">
             <div className="header-content">
-            <img src='../src/assets/1710393763804.jpg' alt="photo" className="header-image" />
+            <img src='https://drive.google.com/uc?export=download&id=1KyoHAuUCFcSe6qZ1FuF18khb1vHDmxIA' alt="photo" className="header-image" />
 
                 <div className="header-text">
                     <h1>CHRISTIAN CONTRERAS</h1>
